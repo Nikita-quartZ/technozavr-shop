0c2f99e7a9f881f83dce756333a4d4c8a3a4d928
Nikita-quartZ <Flashfly16@gmail.com>
2022-04-29 11:01:32 +0300

    update filter

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
+    <div class="content__catalog">
+      <ProductFilter
+        :price-from.sync='FilterPriceFrom'
+        :price-to.sync='FilterPriceTo'
+        :category-id.sync='FilterCategory'
+        :colors-id.sync='FilterColor'
+      />
+      <section class="catalog">
+        <ProductList :products="products"/>
+        <BasePagination
+          v-model="page"
+          :count="amountProducts"
+          :per-page="productPerPage"
+        />
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import products from './data/products';
+import ProductList from './components/ProductList.vue';
+import BasePagination from './components/BasePagination.vue';
+import ProductFilter from './components/ProductFilter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProductList,
+    BasePagination,
+    ProductFilter,
+  },
+  data() {
+    return {
+      page: 1,
+      productPerPage: 3,
+      FilterPriceFrom: 0,
+      FilterPriceTo: 0,
+      FilterCategory: 0,
+      FilterColor: 0,
+    };
+  },
+  computed: {
+    filteredProducts() {
+      let filterProducts = products;
+      if (this.FilterPriceFrom > 0) {
+        filterProducts = filterProducts.filter((product) => product.price > this.FilterPriceFrom);
+      }
+      if (this.FilterPriceTo > 0) {
+        filterProducts = filterProducts.filter((product) => product.price < this.FilterPriceTo);
+      }
+      if (this.FilterColor > 0) {
+        filterProducts = filterProducts.filter((product) => product.colorId === this.FilterColor);
+      }
+      if (this.FilterCategory) {
+        filterProducts = filterProducts
+          .filter((product) => product.categoryId === this.FilterCategory);
+      }
+      return filterProducts;
+    },
+    products() {
+      const offset = (this.page - 1) * this.productPerPage;
+      return this.filteredProducts.slice(offset, offset + this.productPerPage);
+    },
+    amountProducts() {
+      return this.filteredProducts.length;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BasePagination.vue
@@ -0,0 +1,63 @@
+<template>
+  <ul class="catalog__pagination pagination">
+    <li class="pagination__item">
+      <a
+        href="#"
+        class="pagination__link pagination__link--arrow"
+        :class="{'pagination__link--disabled': page === 1}"
+        aria-label="Предыдущая страница"
+        @click.prevent="changePage(page - 1)"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
+      <a
+        href="#"
+        class="pagination__link"
+        :class="{'pagination__link--current': pageNumber === page}"
+        @click.prevent="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow"
+        :class="{'pagination__link--disabled': page === pages}"
+        href="#"
+        aria-label="Следующая страница"
+        @click.prevent="changePage(page + 1)"
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'BasePagiantion',
+  model: {
+    prop: 'page',
+    event: 'paginate',
+  },
+  props: ['page', 'count', 'PerPage'],
+  computed: {
+    pages() {
+      return Math.ceil(this.count / this.PerPage);
+    },
+  },
+  methods: {
+    changePage(page) {
+      if (page <= this.pages && page > 0) {
+        this.$emit('paginate', page);
+      }
+    },
+  },
+};
+</script>
